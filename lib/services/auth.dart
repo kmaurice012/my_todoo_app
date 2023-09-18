0ad70e01ae2993e3c29b8cd4e0afae6e4243c4db
kmaurice012 <kingmaurice012@gmail.com>
2023-09-18 16:00:51 +0300

    WOrking on firesbase Setup and authentication

---
+++ lib/services/auth.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  final FirebaseAuth auth;
+
+  Auth({required this.auth});
+
+  Stream<User?> get user {
+    return auth.authStateChanges();
+  }
+
+  Future<String?> createAccount(String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email.trim(), password: password);
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // ignore: non_constant_identifier_names
+  Future<String?> SignIn(String email, String password) async {
+    try {
+      await auth.signInWithEmailAndPassword(
+          email: email.trim(), password: password.trim());
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String?> SignOut() async {
+    try {
+      await auth.signOut();
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
